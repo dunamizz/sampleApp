ecda88b3f449c86927c219cf99dc86a6457c95cc
dunamizz <powerex4greatness@gmail.com>
2019-10-22 14:13:24 +0200

    Working on Functions

--- src/index.js
+++ src/index.js
@@ -45,3 +45,58 @@ console.log(typeof item.toString());
 // const toString = Object.prototype.toString;
 console.log(toString.call(null));
 // console.log((new Date) .toString());
+console.log('Hello'.length);
+console.log(new String('Hello'));
+console.log (Object.prototype.toString.call('Hello').slice(8,-1));
+console.log (Object.prototype.toString.call(null).includes('Null'));
+console.log (Object.prototype.toString.call(undefined).slice(8,-1));
+console.log(String.prototype);
+console.log(Number.prototype);
+
+function makecarprice() {
+    // return Object.prototype.toString.call(arguments).slice(8, -1);
+    console.log(Object.prototype.toString.call(arguments).slice(8,-1))
+    let newArr;
+    if (Object.prototype.toString.call(arguments).slice(8, -1) === 'Arguments' ) {
+        newArr = Array.from(arguments).reduce((next, previous) => next + previous)
+    }
+    console.log(newArr);
+    // if (arguments.length != 3) {
+    // console.log('This function requires three parameters');
+    // console.log(Object.prototype.valueOf(arguments))
+    // } else {
+    //     console.log(arguments[0], arguments[1], arguments[2])
+    // }
+}
+
+// const arr = [];
+// console.log(Array.isArray(arr));
+
+makecarprice(11, 22, 340);
+
+const nums = [1, 2, 2, 3];
+
+console.log(Object.prototype.toString.call(new Set(nums)));
+
+function makecarpricerest(...all) {
+    let reducer = (accumulator, currentvalue) => accumulator + currentvalue;
+    const total = all.reduce(reducer);
+    console.log(total);
+    if (total > 2000) console.log('All good');
+    else console.log('Fuck')
+}
+
+makecarpricerest(11, 23, 44, 29, 1000);
+
+function makeCarePart(id) {
+    const theid = `CAR_PART_${id}`;
+
+    return function(name) {
+        return `${theid}_${name.toUpperCase()}`
+    };
+}
+
+const carPart = makeCarePart('xy675432');
+console.log(carPart('Left Door'));
+console.log(carPart('Right Door'));
+console.log(carPart('Wind Shield'));
